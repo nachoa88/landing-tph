2180866d46a82930a2d283efee7e1fb75fa3af95
nacho <nacho.albiol88@gmail.com>
2025-01-23 17:59:32 +0100

    feat: improve accesibility + small fix typo in About section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -29,7 +29,7 @@ const aboutContent = [
     <div class="p-0 sm:p-4 max-w-screen-xl mx-auto">
       <div class="relative overflow-hidden bg-gradient-to-b from-stone-100 to-transparent pt-12 sm:pt-24 sm:rounded-lg">
         <div class="text-center">
-          <SectionTitle text="Get no know us," highlight="Restore The Balance" />
+          <SectionTitle text="Get to know us," highlight="Restore The Balance" />
         </div>
         <ContentSection :content="aboutContent" />
       </div>
