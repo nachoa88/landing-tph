985af3b1d186f871ddfdcd1de81bef81ea1da2e3
nacho <nacho.albiol88@gmail.com>
2025-01-23 12:03:23 +0100

    feat: add Header & Carbon Calculator + small hotfix in Home and About views

--- src/views/EcoTrackerView.vue
+++ src/views/EcoTrackerView.vue
@@ -1,22 +1,25 @@
 <script setup>
-import SectionTitle from "@/components/typography/SectionTitle.vue";
 import ContentSection from "@/components/typography/ContentSection.vue";
+import EcoHeader from "@/components/ecotracker/EcoHeader.vue";
+import CarbonCalculator from "@/components/ecotracker/CarbonCalculator.vue";
 
 const content = [
-  "Maybe some content here, who knows?",
-  "I'll attempt to make a Carbon Footprint Tracker/Calculator, but I'm not sure if I'll have time to develop the full logic for this.",
+  "The Carbon Calculator is designed to provide users with a quick and easy way to estimate their environmental impact. By inputting basic data such as energy consumption, car travel, and flights, you can instantly see an approximation of your carbon footprint in kilograms of CO₂ equivalent.",
+  "This tool uses globally recognized conversion factors to ensure accurate results. Whether you're looking to understand your current emissions or explore ways to reduce them, this calculator empowers you to take meaningful steps toward sustainability.",
 ];
 </script>
 
 <template>
   <section class="w-full flex flex-col">
     <div class="p-0 sm:p-4 max-w-screen-xl mx-auto">
-      <div class="relative overflow-hidden bg-gradient-to-b from-stone-100 to-transparent pt-12 sm:pt24 sm:rounded-lg">
-        <div class="p-10 sm:pt-20 text-center">
-          <SectionTitle text="Welcome to" highlight="EcoTracker" />
-        </div>
+      <div class="relative overflow-hidden bg-gradient-to-b from-stone-100 to-transparent pt-12 sm:pt-24 sm:rounded-lg">
+        <EcoHeader />
         <ContentSection :content="content" />
       </div>
+
+      <main class="mx-auto p-0 mb-8 sm:p-4">
+        <CarbonCalculator />
+      </main>
     </div>
   </section>
 </template>
